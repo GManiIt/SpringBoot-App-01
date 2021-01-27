0c070cff761fc86663bbf26f7dfe5f04bfa9d131
Mani <manigovindan06@gmail.com>
2021-01-27 07:46:42 +0530

    added variable

--- src/main/java/com/lara/springboot/SpringBootApp01Application.java
+++ src/main/java/com/lara/springboot/SpringBootApp01Application.java
@@ -8,6 +8,9 @@ public class SpringBootApp01Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootApp01Application.class, args);
+
+		int j =20;
+		//this code changed after stash
 	}
 
 }
