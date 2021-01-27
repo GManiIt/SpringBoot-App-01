b6717ad8d4071656be65a50d514ff49b80a29a76
Mani <manigovindan06@gmail.com>
2021-01-27 08:00:50 +0530

    added variable

--- src/main/java/com/lara/springboot/SpringBootApp01Application.java
+++ src/main/java/com/lara/springboot/SpringBootApp01Application.java
@@ -9,8 +9,7 @@ public class SpringBootApp01Application {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootApp01Application.class, args);
 
-		//code will use
-		int i=10;
+		//stash after using
 	}
 
 }
